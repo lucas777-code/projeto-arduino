af91b73e8994035f8553fe6228cf37ea67521169
Lucas Comparini <luquinhasdelas08@gmail.com>
2024-08-05 15:21:41 -0300

    mudança

--- sketch_aug05a/sketch_aug05a.ino
+++ sketch_aug05a/sketch_aug05a.ino
@@ -1,9 +1,14 @@
+//Programa que envia sinal alto para a saida 7 
+//aguarda para enviar um sinal baixo para a saida 7
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(7,OUTPUT)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+digitalWrite(7,HIGH);
+delay(1000);
+digitalwrite(7,LOW);
+delay(1000); 
 }
